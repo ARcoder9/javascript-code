a68b9d9891a88a56e17e91ae58436e145750b359
Ayush Ranjan <ayush.ranjan.official1@gmail.com>
2024-07-06 23:06:17 +0530

    pull

--- 01-Basics/conversionOperation.js
+++ 01-Basics/conversionOperation.js
@@ -1,13 +1,12 @@
 
 let score = "33"
-<<<<<<< HEAD
 console.log(typeof score);
 // console.log(typeof(score)); // another way to write type of
 
-let valueInNumber = Number(score) 
+let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber)
-=======
+
 // console.log(typeof score);
 // console.log(typeof(score)); // another way to write type of
 
@@ -15,7 +14,6 @@ let valueInNumber = Number(score)
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber)
 
->>>>>>> 5410e38 (conversion of datatypes)
 // "Ayush"   => NaN
 // "33abc"   => NaN
 // null      => 0
@@ -26,40 +24,25 @@ let valueInNumber = Number(score)
 
 let isloggedIn = undefined
 let booleanloggedIn = Boolean(isloggedIn)
-<<<<<<< HEAD
-console.log(typeof booleanloggedIn)
-console.log(booleanloggedIn)
-=======
 // console.log(typeof booleanloggedIn)
 // console.log(booleanloggedIn)
->>>>>>> 5410e38 (conversion of datatypes)
-
 // 1 => true , 0 => false
 // "" => false
 // "hitesh" => true
 // null => false
 // undefined => false
 
-<<<<<<< HEAD
 let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber)
 console.log(stringNumber);
-=======
-let someNumber = undefined
-
-let stringNumber = String(someNumber)
 // console.log(typeof stringNumber)
 // console.log(stringNumber);
->>>>>>> 5410e38 (conversion of datatypes)
 
 // 33 => "33"
 // true => "true" , false => "false"
 // null => "null" , undefine => "undefined"
 
-<<<<<<< HEAD
-
-=======
 // ************************ operations ************************
 
 let value = 3
@@ -81,7 +64,6 @@ let str2 =" Ranjan"
 // console.log(1 + "2")
 // console.log("1" + 2 + 2)
 // console.log(1 + 2 + "2")
->>>>>>> 5410e38 (conversion of datatypes)
 
 
 

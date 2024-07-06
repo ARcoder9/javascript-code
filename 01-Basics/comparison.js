1dc15d90ee472d34a6b5028e99abc176396df900
Ayush Ranjan <ayush.ranjan.official1@gmail.com>
2024-07-06 23:19:12 +0530

    comparison

--- 01-Basics/comparison.js
+++ 01-Basics/comparison.js
@@ -1,2 +1,21 @@
-console.log(2 > 1)
-console.log
+// console.log(2 > 1)
+// console.log(2 >=1 )
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+// console.log("2" > 1); //whenever compare two values always make the datatype same.
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// console.log(undefined == 0)
+// console.log(undefined < 0)
+// console.log(undefined > 0)
+
+// ===  strict check
+
+console.log("2" === 2) //it checks the value as well as the datatype so it doesn't convert the datatype of value
+
+

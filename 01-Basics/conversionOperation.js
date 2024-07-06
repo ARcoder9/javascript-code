05ce5f6e5a804f5d193af86794fe62a95b4e1d0e
Ayush Ranjan <ayush.ranjan.official1@gmail.com>
2024-07-06 22:57:47 +0530

    conversion of datatypes

--- 01-Basics/conversionOperation.js
+++ 01-Basics/conversionOperation.js
@@ -1,11 +1,21 @@
 
 let score = "33"
+<<<<<<< HEAD
 console.log(typeof score);
 // console.log(typeof(score)); // another way to write type of
 
 let valueInNumber = Number(score) 
 console.log(typeof valueInNumber);
 console.log(valueInNumber)
+=======
+// console.log(typeof score);
+// console.log(typeof(score)); // another way to write type of
+
+let valueInNumber = Number(score) 
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+>>>>>>> 5410e38 (conversion of datatypes)
 // "Ayush"   => NaN
 // "33abc"   => NaN
 // null      => 0
@@ -16,8 +26,13 @@ console.log(valueInNumber)
 
 let isloggedIn = undefined
 let booleanloggedIn = Boolean(isloggedIn)
+<<<<<<< HEAD
 console.log(typeof booleanloggedIn)
 console.log(booleanloggedIn)
+=======
+// console.log(typeof booleanloggedIn)
+// console.log(booleanloggedIn)
+>>>>>>> 5410e38 (conversion of datatypes)
 
 // 1 => true , 0 => false
 // "" => false
@@ -25,16 +40,48 @@ console.log(booleanloggedIn)
 // null => false
 // undefined => false
 
+<<<<<<< HEAD
 let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber)
 console.log(stringNumber);
+=======
+let someNumber = undefined
+
+let stringNumber = String(someNumber)
+// console.log(typeof stringNumber)
+// console.log(stringNumber);
+>>>>>>> 5410e38 (conversion of datatypes)
 
 // 33 => "33"
 // true => "true" , false => "false"
 // null => "null" , undefine => "undefined"
 
+<<<<<<< HEAD
 
+=======
+// ************************ operations ************************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue) // output is -3
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "Ayush"
+let str2 =" Ranjan"
+// console.log(str1+str2)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+>>>>>>> 5410e38 (conversion of datatypes)
 
 
 

c098bbdb95d197a5208ea9b6c570c8f384ca286f
Ayush Ranjan <ayush.ranjan.official1@gmail.com>
2024-07-07 12:52:00 +0530

    memory

--- 01-Basics/datatypes-summary.js
+++ 01-Basics/datatypes-summary.js
@@ -11,3 +11,29 @@ JavaScript is a dynamically typed language.
 This means that the type of a variable is determined at runtime, not at compile time. 
 You don't need to explicitly declare the type of a variable when you create it. 
 */
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack , Heap
+
+// primitive datatypes uses stack memory
+// non - primitive datatypes uses heap memory
+
+let myName = "Ayush"
+let anotherName = myName
+anotherName = "Ranjan"
+
+console.log(myName)
+console.log(anotherName)
+
+let userOne = {
+    email: "user1@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "user2@google.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
